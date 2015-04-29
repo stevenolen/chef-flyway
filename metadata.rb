92ac486e3ecec50091f25817575fa0fe3a853024
Steve Nolen <technolengy@gmail.com>
2015-04-29 11:25:46 -0700

    empty cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'flyway'
+maintainer       'Steve Nolen'
+maintainer_email 'technolengy@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures flyway'
+long_description 'Installs/Configures flyway'
+version          '0.1.0'
